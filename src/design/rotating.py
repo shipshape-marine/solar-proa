4ceccaf26da576354ab4530ef93d587cf427422f
Christopher Tan <christopher.tzr@u.nus.edu>
2026-09-15 18:59:25 +0800

    Check reefing percentage value

--- src/design/rotating.py
+++ src/design/rotating.py
@@ -15,16 +15,19 @@ def rig(the_rig, params, sail_angle=0, sail_camber=10000, reefing_percentage=0,
     x_offset, y_offset: absolute position of the mast base
     z_rotation: rotation around Z axis in degrees
     """
-    # check camber value
     if sail_camber <= 0:
         raise ValueError("Value of sail_camber parameter must be greater than 0")
 
-    # height of stretched out sail after reefing
+    if not 0 <= reefing_percentage < 100:
+        # if reefing % = 100, freecad will try to make box of height=0 and throws height too small error
+        raise ValueError("Value of reefing_percentage parameter must be at least 0 and less than 100")
+    
+    # Height of stretched-out sail after reefing
     reefed_sail_height = params['sail_height'] * (100 - reefing_percentage) / 100
-    yard_boom_distance = 2 * sail_camber * math.sin(reefed_sail_height / (2 * sail_camber))
+    central_angle = reefed_sail_height / sail_camber
 
-    # angle formed by the sail arc cannot be more than pi (arc cannot exceed a semicircle)
-    if (reefed_sail_height / sail_camber) >= math.pi:
+    # The sail arc cannot exceed a semicircle
+    if central_angle >= math.pi:
         minimum_camber = reefed_sail_height / math.pi
         minimum_reefing = 100 * (
             1 - math.pi * sail_camber / params['sail_height']
@@ -35,6 +38,8 @@ def rig(the_rig, params, sail_angle=0, sail_camber=10000, reefing_percentage=0,
             f"Increase sail_camber above {minimum_camber:.1f} mm "
             f"or reefing_percentage above {minimum_reefing:.1f}%."
         )
+
+    yard_boom_distance = 2 * sail_camber * math.sin(central_angle / 2)
 
     # Base position for this rig
     base_x = x_offset
